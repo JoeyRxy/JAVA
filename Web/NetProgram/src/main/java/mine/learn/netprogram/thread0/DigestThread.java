30bec68d44c1495a1afb70c10f99dffed0f82def
joey <joeyrxy@gmail.com>
2019-09-29 23:03:30 +0800

    ok

---
+++ Web/NetProgram/src/main/java/mine/learn/netprogram/thread0/DigestThread.java
@@ -0,0 +1,42 @@
+package mine.learn.netprogram.thread0;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * DigestThread
+ */
+public class DigestThread extends Thread {
+
+    private String filename;
+
+    public DigestThread(String filename) {
+        this.filename = filename;
+    }
+
+    @Override
+    public void run() {
+        try {
+            FileInputStream in = new FileInputStream(filename);
+            MessageDigest sha = MessageDigest.getInstance("sha-256");
+            DigestInputStream digestInputStream = new DigestInputStream(in, sha);
+            while (digestInputStream.read() != -1)
+                ;
+            digestInputStream.close();
+            byte[] digest = sha.digest();
+
+            String res = new String(filename + ": ");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
